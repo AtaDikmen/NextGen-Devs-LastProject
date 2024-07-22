5f4c3d48fa1c6de1ec743aa85c8b250dee844faa
Can Usluel <canusluel@gmail.com>
2024-07-23 02:22:58 +0300

    changed models in merge ui

--- Assets/Scripts/GridManager.cs
+++ Assets/Scripts/GridManager.cs
@@ -6,12 +6,27 @@ using UnityEngine.UI;
 
 public class GridManager : MonoBehaviour
 {
-    public GameObject troopImagePrefab;
+    public GameObject troopImageParentPrefab;
+    public List<GameObject> troopImagePrefabs;
     public GridLayoutGroup gridPanel;
     private List<TroopImage> troopImages;
     [SerializeField] private int gridSize = 16;
     [SerializeField] private int spawnCount = 2;
 
+    public static GridManager Instance;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
     void Start()
     {
         troopImages = new List<TroopImage>();
@@ -21,7 +36,7 @@ public class GridManager : MonoBehaviour
     {
         for (int i = 0; i < gridSize; i++)
         {
-            GameObject newTroop = Instantiate(troopImagePrefab, gridPanel.gameObject.transform);
+            GameObject newTroop = Instantiate(troopImageParentPrefab, gridPanel.gameObject.transform);
             TroopImage newTroopImage = newTroop.GetComponent<TroopImage>();
             troopImages.Add(newTroopImage);
             newTroopImage.Initialize(TroopType.LaserSword);
@@ -37,10 +52,13 @@ public class GridManager : MonoBehaviour
             {
                 troopImages[i].ActivateChild();
                 tempSpawnCount++;
-                AudioClip spawnSFX = Resources.Load<AudioClip>("Spawn");
-                AudioManager.Instance.PlaySFX(spawnSFX);
             }
             i++;
+        }
+        if (tempSpawnCount > 0)
+        {
+            AudioClip declineMergeSFX = Resources.Load<AudioClip>("Spawn");
+            AudioManager.Instance.PlaySFX(declineMergeSFX);
         }
 
     }
