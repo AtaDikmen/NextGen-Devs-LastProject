6ad0f6b6463ff36f192e55b85762fd7eef57de17
Can Usluel <canusluel@gmail.com>
2024-07-25 23:00:46 +0300

    fixed spawn bug on new game

--- Assets/Scripts/UI/MergeUI/GridManager.cs
+++ Assets/Scripts/UI/MergeUI/GridManager.cs
@@ -19,7 +19,6 @@ public class GridManager : MonoBehaviour
         if (Instance == null)
         {
             Instance = this;
-            DontDestroyOnLoad(gameObject);
         }
         else
         {
